ff8f0bcfc49adee736b45fbf9d0e9bf656bf8eb6
dijoncavalcante <dijoncavalcante@gmail.com>
2021-03-17 13:43:20 -0400

    Add control flow

--- app/src/main/java/com/dijon/diceroller/MainActivity.kt
+++ app/src/main/java/com/dijon/diceroller/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+
 /**
  * This activity allows the user to roll a dice and view the result
  * on the screen.
@@ -16,6 +17,7 @@ class MainActivity : AppCompatActivity() {
         val rollButton: Button = findViewById(R.id.button)
         rollButton.setOnClickListener { rollDice() }
     }
+
     /**
      * Roll the dice and update the screen with the result.
      */
@@ -23,10 +25,23 @@ class MainActivity : AppCompatActivity() {
         // Create new Dice object with 6 sides and roll it
         val dice = Dice(6)
         val diceRoll = dice.roll()
+        val luckyNumber = 4
+
+        var message = "fail"
+
+        when (diceRoll) {
+            //luckyNumber -> println("")
+            1 -> message = "So sorry! You rolled a 1. Try again!"
+            2 -> message = "Sadly, you rolled a 2. Try again!"
+            3 -> message = "Unfortunately, you rolled a 3. Try again!"
+            4 -> message = "You won!"
+            5 -> message = "Don't cry! You rolled a 5. Try again!"
+            6 -> message = "Apologies! you rolled a 6. Try again!"
+        }
 
         // Update the screen with the dice roll
         val resultTextView: TextView = findViewById(R.id.textView)
-        resultTextView.text = diceRoll.toString()
+        "$luckyNumber \n $message".also { resultTextView.text = it }
     }
 }
 
